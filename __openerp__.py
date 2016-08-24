e3019ad0561e9c49fc4a3b97cb30defb4f54362c
Thonh <thonh.hanelsoft.vn>
2016-08-24 11:34:58 +0700

    update description

--- __openerp__.py
+++ __openerp__.py
@@ -1,25 +1,16 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-
 {
-    'name': 'Restaurant',
+    'name': 'Restaurant Extension',
+    'summary': 'Allow to transfer/ merge/ split table; Allow to connect directly to printer by using Order function',
     'version': '1.0',
     'category': 'Point of Sale',
-    'sequence': 6,
-    'summary': 'Restaurant extensions for the Point of Sale ',
     'description': """
-
-=======================
-
-This module adds several restaurant features to the Point of Sale:
-- Bill Printing: Allows you to print a receipt before the order is paid
-- Bill Splitting: Allows you to split an order into different orders
-- Kitchen Order Printing: allows you to print orders updates to kitchen or bar printers
-
-""",
+    """,
+    'author': "Hanel Software Solutions",
+    'website': 'http://www.hanelsoft.vn/',
     'depends': ['point_of_sale'],
-    'website': 'https://www.odoo.com/page/point-of-sale',
     'data': [
         'restaurant_view.xml',
         'security/ir.model.access.csv',
@@ -37,4 +28,6 @@ This module adds several restaurant features to the Point of Sale:
     ],
     'installable': True,
     'auto_install': False,
+    'price': 39.99,
+    'currency': 'EUR',
 }
